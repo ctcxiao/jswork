71bc78f657a595370f8eeb2e49140d2f693d1591
tcchen <tcchen@thoughtworks.com>
2018-09-04 16:47:48 +0800

    [Cheng] commit practice 5.

--- src/practice_5/person.js
+++ src/practice_5/person.js
@@ -1,1 +1,15 @@
+export default class Person {
+    name;
+    age;
 
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    introduce() {
+        return 'My name is Tom. I am 21 years old.';
+    }
+
+
+}
