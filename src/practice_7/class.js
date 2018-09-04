fdc0ab06c2c9913d9686f7d86504035abe77e849
tcchen <tcchen@thoughtworks.com>
2018-09-04 16:48:33 +0800

    [Cheng] commit practice 7.

--- src/practice_7/class.js
+++ src/practice_7/class.js
@@ -1,1 +1,11 @@
+export default class Class {
+    number;
 
+    constructor(number) {
+        this.number = number;
+    }
+
+    getDisplayName() {
+        return "Class " + this.number;
+    }
+}
--- src/practice_7/student.js
+++ src/practice_7/student.js
@@ -1,1 +1,20 @@
+import Person from "./person";
 
+export default class Student extends Person{
+    name;
+    age;
+    klass;
+
+    constructor(name, age, klass) {
+        super(name, age);
+        this.name = name;
+        this.age = age;
+        this.klass = klass;
+    }
+
+    introduce() {
+        return super.introduce()+' I am a Student. I am at Class 2.';
+    }
+
+
+}
