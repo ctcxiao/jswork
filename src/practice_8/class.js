303630369fcd3dbd80ad58c529477c8a87446ee1
tcchen <tcchen@thoughtworks.com>
2018-09-04 16:48:51 +0800

    [Cheng] commit practice 9.

--- src/practice_8/class.js
+++ src/practice_8/class.js
@@ -1,1 +1,16 @@
+export default class Class {
+    number;
+    leader;
 
+    constructor(number) {
+        this.number = number;
+    }
+
+    getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    assignLeader(leader){
+        this.leader = leader;
+    }
+}
--- src/practice_8/person.js
+++ src/practice_8/person.js
@@ -1,1 +1,17 @@
+export default class Person {
 
+    name;
+    age;
+    klassNumber;
+
+    constructor(klassNumber, name, age) {
+        this.name = name;
+        this.age = age;
+        this.klassNumber = klassNumber;
+    }
+
+    introduce() {
+        return 'My name is Tom. I am 21 years old.';
+    }
+
+}
