6ea1dd4b7545ca98644bf8e73479b8609c86e9dd
tcchen <tcchen@thoughtworks.com>
2018-09-04 16:46:15 +0800

    [Cheng] commit practice 1.

---
+++ src/practice_6/worker.js
@@ -0,0 +1,16 @@
+import Person from "./person";
+
+export default class Worker extends Person{
+    name;
+    age;
+
+    constructor(name, age, klass) {
+        super(name, age);
+        this.name = name;
+        this.age = age;
+    }
+
+    introduce() {
+        return super.introduce()+' I am a Teacher. I have a job.';
+    }
+}
